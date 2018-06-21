c9f9346da8c920ebce094290487258d7e5f35a58
breakfastmeansbreakfast <77744c@gmail.com>
2018-06-21 20:06:19 +0100

    updated jest tests

--- __tests__/test1.js
+++ __tests__/test1.js
@@ -6,6 +6,32 @@ describe('constructor', () => {
     it ('checks for pet name', () => {
         expect(new Pet('Fido')).toHaveProperty('name', 'Fido')
 
+    });
+    it ('checks correct age', () => {
+        expect(new Pet('Fido')).toHaveProperty('age', 0)
     })
 }
+)
+
+describe('growUp', () => {
+    it('increments the age by 1', () => {
+      const pet = new Pet('Fido');
+      pet.growUp();
+      expect(pet.age).toEqual(1);
+    });
+  });
+  
+describe('hunger and fitness', () => {
+    it('checks whether hunger has increased', () => {
+        const pet = new Pet('Chris');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
+    })
+    it('checks fitness has decreased', () => {
+        const pet2 = new Pet ('Giles');
+        pet2.growUp();
+        expect(pet2.fitness).toEqual(7)
+    })
+}
+
 )
--- src/test1.js
+++ src/test1.js
@@ -1,6 +1,25 @@
 // const Pet = require('../src/test1');
 function Pet(name) {
+    this.age = 0,
+    this.hunger = 0,
+    this.fitness = 10,
     this.name = name
+/*    this.growUp = function () {
+        this.age += 1
+    }*/
+    } 
+Pet.prototype.growUp = function() {
+    this.age +=1
+    this.hunger +=5
+    this.fitness -=3
 }
+
+/*Pet.prototype.walk = function() {
+    if(this.fitness > 6) {
+        this.fitness 
+    } else {
+    this.fitness +=4
+    }
+}*/
 
 module.exports = Pet;
